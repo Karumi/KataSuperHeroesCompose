ae25ac2619076c3dea972903afe9a181fa7ce0dc
Pedro Gómez <pedro@karumi.com>
2021-06-16 10:34:51 +0200

    Configure dispatcher

--- app/src/androidTest/java/com/karumi/utils/MockedTest.kt
+++ app/src/androidTest/java/com/karumi/utils/MockedTest.kt
@@ -1,11 +1,14 @@
 package com.karumi.utils
 
+import com.karumi.core.ui.ViewModel
+import kotlinx.coroutines.Dispatchers
 import org.junit.Before
 import org.mockito.MockitoAnnotations
 
 interface MockedTest {
     @Before
     fun setUp() {
+        ViewModel.dispatcher = Dispatchers.Unconfined
         MockitoAnnotations.initMocks(this)
     }
 }
